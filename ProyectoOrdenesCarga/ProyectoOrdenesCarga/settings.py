700204d07f79ac18ad2220021d402b1e1019f17b
lmanucci <lautaromanucci@gmail.com>
2022-08-23 23:33:36 -0300

    listo

--- ProyectoOrdenesCarga/ProyectoOrdenesCarga/settings.py
+++ ProyectoOrdenesCarga/ProyectoOrdenesCarga/settings.py
@@ -77,14 +77,14 @@ WSGI_APPLICATION = 'ProyectoOrdenesCarga.wsgi.application'
 
 db_from_env= dj_database_url.config(conn_max_age=500)
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+ }
 
-DATABASES =['default'].update(db_from_env)
+#DATABASES =['default'].update(db_from_env)
 
 
 # Password validation
